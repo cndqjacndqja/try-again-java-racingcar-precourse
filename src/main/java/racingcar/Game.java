2b5d95f72bab167aae2a4def02e05c62097378ff
jochungbeom <cndqjacndqja@gmail.com>
2021-12-01 16:40:34 +0900

    feat: 각 자동차들 이동

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import racingcar.domain.CarRepository;
 import racingcar.view.InputView;
 
 public class Game {
 	public void run() {
 		String[] carNames = InputView.inputCarNames();
-
+		CarRepository carRepository = new CarRepository();
+		carRepository.createCars(carNames);
+		int numberOfCarsMove = InputView.inputNumberOfCarsMoving();
+		System.out.println("실행 결과");
+		for (int i = 0; i < numberOfCarsMove; i++) {
+			carRepository.moveCars();
+		}
 	}
 }
